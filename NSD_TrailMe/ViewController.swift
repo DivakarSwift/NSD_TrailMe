a6f5b353bc02868a47cd6582657a82086985bf3c
Nathaniel Coleman <nathanielcoleman@nathaniels-macbook-pro.local>
2018-11-19 22:48:12 -0500

    Initial Commit

---
+++ NSD_TrailMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NSD_TrailMe
+//
+//  Created by Nathaniel Coleman on 11/19/18.
+//  Copyright © 2018 Nathaniel Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
